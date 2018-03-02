191b36f5e833becab251d707c7c28901607a280d
Alain <Alain@25.13.86.247>
2018-03-01 19:08:45 -0700

    adds sound statement

--- SudokuHW2/src/sudoku/dialog/SudokuDialog.java
+++ SudokuHW2/src/sudoku/dialog/SudokuDialog.java
@@ -5,8 +5,15 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -87,8 +94,20 @@ public class SudokuDialog extends JFrame {
             showMessage("Number clicked: " + number);
     	}
     	else {
-    		//playsound
-    	}
+    		 try {           
+    	          File soundFile = new File("");
+    	          AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);              
+    	         Clip clip = AudioSystem.getClip();
+    	         clip.open(audioIn);
+    	         clip.start();
+    	      } catch (UnsupportedAudioFileException e) {
+    	         e.printStackTrace();
+    	      } catch (IOException e) {
+    	         e.printStackTrace();
+    	      } catch (LineUnavailableException e) {
+    	         e.printStackTrace();
+    	      }
+    	   }
     	if(board.checkSolved()) {
     		int selectedOption = JOptionPane.showConfirmDialog(msgBar, "Congratulations!! Start new game?", "Solved!", JOptionPane.YES_NO_OPTION);
     		if(selectedOption == JOptionPane.YES_OPTION) {
