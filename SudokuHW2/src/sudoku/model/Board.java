aa1d28ccd27be49b43da7d27d3dcc7c79bd69cae
germain40 <germain.v40@gmail.com>
2018-04-10 19:12:55 -0600

    Update Board.java

--- SudokuHW2/src/sudoku/model/Board.java
+++ SudokuHW2/src/sudoku/model/Board.java
@@ -19,11 +19,6 @@ public class Board {
     }
 	/** updates board and checks valid entry */
 	public int[][] checkBoard(int n, int x, int y) {
-		int sqrt = (int) Math.sqrt(b.length);
-		int subx = x / sqrt;
-		int suby = y / sqrt;
-		int boundx = (subx +1) * sqrt;
-		int boundy = (suby +1) * sqrt;
 		
 		if (n == 0) {
 			b[x][y] = 0;
@@ -33,34 +28,56 @@ public class Board {
 			if (x >= 0 && x <= b.length) {
 				if (y >= 0 && y <= b.length) {
 					/** checks rules */
-					for (int i = 0; i < b.length; i++) {
-						if (b[x][i] == n) {
-							System.out.println("Invalid input... Number in same row");
-							return b;
-						}
-						
-						if (b[i][y] == n) {
-							System.out.println("Invalid Input... Number in same column");
-							return b;
-						}
-					}
-					/** checks rules in subgrids */
-					for (int i = subx * sqrt; i < boundx; i++) {
-						for (int j = suby * sqrt; j < boundy; j++) {
-							if (b[i][j] == n) {
-								System.out.println("Invalid Input.. Number in same Sub-Grid");
-								return b;
+					if (checkRow()) {
+						if (checkColum()) {
+							if (checkSub()) {
+								b[x][y] = n;
+								//System.out.println("Updated!");
 							}
 						}
 					}
-					
-					b[x][y] = n;
-					//System.out.println("Updated!");
 				}
 			}
 		}
 		return b;
 	}
+	
+	public boolean checkColumn() {
+		for (int i = 0; i < b.length; i++) {
+			if (b[i][y] == n) {
+				System.out.println("Invalid Input... Number in same column");
+				return false;
+			}
+		}
+		return true;
+	}
+	public boolean checkRow() {
+		for (int i = 0; i < b.length; i++) {
+			if (b[x][i] == n) {
+				System.out.println("Invalid input... Number in same row");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean checkSub() {
+		int sqrt = (int) Math.sqrt(b.length);
+		int subx = x / sqrt;
+		int suby = y / sqrt;
+		int boundx = (subx +1) * sqrt;
+		int boundy = (suby +1) * sqrt;
+		for (int i = subx * sqrt; i < boundx; i++) {
+			for (int j = suby * sqrt; j < boundy; j++) {
+				if (b[i][j] == n) {
+					System.out.println("Invalid Input.. Number in same Sub-Grid");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
 	/** checks if board is solved */
 	public boolean checkSolved() {
 		for (int i = 0; i < b.length; i++) {
