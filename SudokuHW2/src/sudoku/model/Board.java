b3d0e43992e46517e03e9214de3ce0d122d56a3b
alain1227 <36968690+alain1227@users.noreply.github.com>
2018-04-10 20:00:31 -0600

    Update Board.java

--- SudokuHW2/src/sudoku/model/Board.java
+++ SudokuHW2/src/sudoku/model/Board.java
@@ -9,8 +9,11 @@ public class Board {
 
     /** Create a new board of the given size. */
     public Board(int size) {
+    	Solve solver = new Solve(Board.this, size);
         this.size = size;
         b = new int[size][size];
+        
+       //solver.solve();
     }
 
     /** Return the size of this board. */
@@ -19,22 +22,18 @@ public class Board {
     }
 	/** updates board and checks valid entry */
 	public int[][] checkBoard(int n, int x, int y) {
-		
 		if (n == 0) {
 			b[x][y] = 0;
 			return b;
 		}
+		
 		if (n > 0 && n <= b.length) {
 			if (x >= 0 && x <= b.length) {
 				if (y >= 0 && y <= b.length) {
 					/** checks rules */
-					if (checkRow(x)) {
-						if (checkColum(y)) {
-							if (checkSub(x, y)) {
-								b[x][y] = n;
-								//System.out.println("Updated!");
-							}
-						}
+					if(checkValidCoordinates(n, x, y)) {
+						setCoordinates(n, x, y);
+						//System.out.println("Updated!");
 					}
 				}
 			}
@@ -42,26 +41,25 @@ public class Board {
 		return b;
 	}
 	
-	public boolean checkColumn(int y) {
+	public boolean checkValidCoordinates(int n, int x, int y) {
 		for (int i = 0; i < b.length; i++) {
 			if (b[i][y] == n) {
-				System.out.println("Invalid Input... Number in same column");
+				//System.out.println("Invalid Input... Number in same column");
 				return false;
 			}
 		}
-		return true;
-	}
-	public boolean checkRow(int x) {
 		for (int i = 0; i < b.length; i++) {
 			if (b[x][i] == n) {
-				System.out.println("Invalid input... Number in same row");
+				//System.out.println("Invalid input... Number in same row");
 				return false;
 			}
 		}
-		return true;
-	}
-	
-	public boolean checkSub(int x, int y) {
+		for (int i = 0; i < b.length; i++) {
+			if (b[x][i] == n) {
+				//System.out.println("Invalid input... Number in same row");
+				return false;
+			}
+		}
 		int sqrt = (int) Math.sqrt(b.length);
 		int subx = x / sqrt;
 		int suby = y / sqrt;
@@ -70,12 +68,16 @@ public class Board {
 		for (int i = subx * sqrt; i < boundx; i++) {
 			for (int j = suby * sqrt; j < boundy; j++) {
 				if (b[i][j] == n) {
-					System.out.println("Invalid Input.. Number in same Sub-Grid");
+					//System.out.println("Invalid Input.. Number in same Sub-Grid");
 					return false;
 				}
 			}
 		}
 		return true;
+	}
+	
+	public void setCoordinates(int n, int x, int y) {
+		b[x][y] = n;
 	}
 	
 	/** checks if board is solved */
@@ -89,7 +91,17 @@ public class Board {
 		return true;
 	}
 	
+	public void setBoardArray(int[][] a) {
+		for (int i = 0; i < size; i++) 
+			for (int j = 0; j < size; j++)
+				b[i][j] = a[i][j];
+	}
+	
 	public int[][] getBoard() {
 		return b;
+	}
+	
+	public int getSize() {
+		return size;
 	}
 }
