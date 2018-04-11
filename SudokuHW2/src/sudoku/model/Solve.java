34a5077049d75a7b54f23b274230de301bc014a9
germain40 <germain.v40@gmail.com>
2018-04-11 10:48:16 -0600

    Update Solve.java

--- SudokuHW2/src/sudoku/model/Solve.java
+++ SudokuHW2/src/sudoku/model/Solve.java
@@ -5,54 +5,64 @@ public class Solve {
 	int size;
 	
 	public Solve(Board b, int s){
-		board = b;
-		size = s;
+		this.board = b;
+		this.size = s;
 		
 	}
 	
-	public boolean solve(){
-		return solve(0, 0);
-	}
 	/**
 	 * 
 	 * @param x
 	 * @param y
 	 * @return
 	 */
-	public boolean solve(int x, int y){
-		int xN, yN; //new x and y
-		if(x == size-1){
-			xN = 0; 
-			yN = y + 1;
-		}
-		else{
-			xN = x + 1;
-			yN = y;
-		}
+	public boolean solve(){
+		XY xy = new XY (0,0);
+		int grid[][] = board.getBoard();
 		
-		if(board.checkSolved()) 	return true;
-		else{
-			for (int i = 1; i <= size; ++i){
-				int[] ca = {i,x,y};
-				if (board.checkValidCoordinates(i, x, y)){
-					board.setCoordinates(i, x, y);
-					if(solve(xN, yN)) return true;
-					else{
-						ca[0] = 0;
-						ca[1] = x;
-						ca[2] = y;
-						board.setCoordinates(i, x, y);
+		if (checkNums(grid, xy)) {
+			board.setBoardArray(grid);
+			return true;
+		} else {
+			for (int n = 1; n <= size; n++) {
+				if (board.checkValidCoordinates(n, xy.x, xy.y)) {
+					grid[xy.x][xy.y] = n;
+					if(solve()) {
+						return true;
+					} else {
+						grid[xy.x][xy.y] = 0;
 					}
 				}
 			}
 		}
 		return false;
-		
 	}
+	
 	public boolean isSolvable(){
 		Board tempBoard = new Board(size);
 		tempBoard.setBoardArray(board.getBoard());
 		Solve tempsolve = new Solve(tempBoard, tempBoard.size);
 		return tempsolve.solve();
 	}
+	
+	private static boolean checkNums(int[][] g, XY rc) {
+		for (rc.x = 0; rc.x < g.length; rc.x++) {
+			for (rc.y =0; rc.y < g.length; rc.y++) {
+				if (g[rc.x][rc.y] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private static class XY {
+        int x;
+        int y;
+
+        XY(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
