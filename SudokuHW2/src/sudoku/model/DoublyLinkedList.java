1986958835b22c572f9aafa38405a55ee218e63a
alain1227 <36968690+alain1227@users.noreply.github.com>
2018-04-15 23:33:32 -0600

    Update DoublyLinkedList.java

--- SudokuHW2/src/sudoku/model/DoublyLinkedList.java
+++ SudokuHW2/src/sudoku/model/DoublyLinkedList.java
@@ -6,6 +6,7 @@ public class DoublyLinkedList {
     private Node current;
     private Node tail;
     private int size;
+    public int x, y, n;
      
     public DoublyLinkedList() {
         size = 0;
@@ -13,8 +14,8 @@ public class DoublyLinkedList {
     
     public int size() { return size; }
 
-    public void add(Board element) {   
-        Node tmp = new Node(element, null, current);
+    public void add(int x, int y, int n) {   
+        Node tmp = new Node(x, y, n, null, current);
         if(current != null) {current.next = tmp;}
         tail = tmp;
         current = tmp;
@@ -22,27 +23,27 @@ public class DoublyLinkedList {
         size++;
     }
     
-    public Board redo(){
+   public void redo(){
     	if(current.next != null){
             current = current.next;
         }
-    	return current.element;
     }
     
-	public Board undo(){
+	public void undo(){
         if(current.prev != null){
             current = current.prev;
         }
-        return current.element;
     }
     
     private class Node {
-        Board element;
+        int x, y, n;
         Node next;
         Node prev;
  
-        public Node(Board element, Node next, Node prev) {
-            this.element = element;
+        public Node(int x, int y, int n, Node next, Node prev) {
+            this.x = x;
+            this.y = y;
+            this.n = n;
             this.next = next;
             this.prev = prev;
         }
